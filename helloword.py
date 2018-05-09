d8f11e8e121e5e650f887f17c80f363031f7d4a9
amitbanik <amit46banik@gmail.com>
2018-05-09 17:57:30 +0530

    removing comments from the bottom

--- helloword.py
+++ helloword.py
@@ -1,15 +1,17 @@
 def main():
-    message="Hello World in meesage"
+    message="Hello World in message"
     print(message)
-    print ( 1 + 2 )
+    print( 1 + 2 )
+
 
 if __name__ == "__main__":
     main()
 
+
 print ("Hello World")
 b = 1
 type(b)
-#a = input("Enter a number: ")
+
 a = 1
 type(a)
 if a == 1:
@@ -31,5 +33,4 @@ print(colors[::-5])
 print(colors[:3:])
 print(colors[:-2])
 
-#for index, item in enumerate(colors):
- #   print("Index: ",index, "Value: ", item)
+
